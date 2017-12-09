37f3f76d07f22c77cfa59012330db1c6f7805ea0
AndrGen <31141640+AndrGen@users.noreply.github.com>
2017-12-10 00:04:32 +0300

    add new test

--- src/test/java/local/CurrStore/CTest.java
+++ src/test/java/local/CurrStore/CTest.java
@@ -1,28 +1,31 @@
 package local.CurrStore;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.text.ParseException;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("CuurencyTest")
-public class CTest {
-    Main main;
+class CTest {
 
     private final String fileNamePart = "src\\test\\";
 
     @Nested
-    @DisplayName("FileReaderConfigCur")
+    @DisplayName("FileReaderTestConfigCur")
     class FileReaderTestConfigCur {
-        private local.CurrStore.FileReader fileReader;
+        private FileReader fileReader;
 
         @BeforeEach
-        void createNewFileReaderConfigCur() {
+        void createNewFileReader() {
             fileReader = new FileReaderConfigCur();
         }
 
@@ -53,12 +56,12 @@ public class CTest {
     }
 
     @Nested
-    @DisplayName("FileReaderInitCur")
+    @DisplayName("FileReaderTestInitCur")
     class FileReaderTestInitCur {
-        private local.CurrStore.FileReader fileReader;
+        private FileReader fileReader;
 
         @BeforeEach
-        void createNewFileReaderConfigCur() {
+        void createNewFileReader() {
             fileReader = new FileReaderInitCur();
         }
 
@@ -72,6 +75,53 @@ public class CTest {
         @DisplayName("fileReader is not empty")
         void fileIsNotEmpty() {
             assertFalse(fileReader == null);
+        }
+    }
+
+    @Nested
+    @DisplayName("CurPayTest")
+    class CurPayTest {
+        private CurPay curPay;
+
+        @BeforeEach
+        void createNewCurPay() {
+            curPay = new CurPay();
+        }
+
+        @org.junit.jupiter.api.Test
+        @DisplayName("throws FileNotFoundException when Incorrect File Name")
+        void throwsExceptionWhenIncorrectFileName() {
+            assertThrows(FileNotFoundException.class, () -> curPay.initCurFromFile(""));
+        }
+
+        @org.junit.jupiter.api.Test
+        @DisplayName("curPay is not empty")
+        void fileIsNotEmpty() {
+            assertFalse(curPay == null);
+        }
+
+        @org.junit.jupiter.api.Test
+        @DisplayName("currency list and currency map is not empty")
+        void currencyIsNotEmpty() throws FileNotFoundException, ParseException {
+            curPay.initCurFromFile(fileNamePart);
+            ConcurrentHashMap<String, Currency> currencyMap = CurPay.getCurrencyMap();
+            assertAll("currencyMapIsNotEmpty",
+                    () -> assertFalse(currencyMap.isEmpty()),
+                    () -> assertTrue(currencyMap.size() == 5),
+                    () -> assertTrue(currencyMap.containsKey("USD")),
+                    () -> assertEquals(new BigDecimal(0), currencyMap.get("AUD").getBill()));
+
+        }
+
+        @org.junit.jupiter.api.Test
+        @DisplayName("isCurCodeInAccess normal input")
+        void isCurCodeInAccessTestNormal() throws FileNotFoundException, ParseException  {
+            curPay.initCurFromFile(fileNamePart);
+            assertAll("isCurCodeInAccessTestNormal",
+                    () -> assertTrue(curPay.isCurCodeInAccess("USD ")),
+                    () -> assertTrue(curPay.isCurCodeInAccess("rub 1")),
+                    () -> assertTrue(curPay.isCurCodeInAccess("rub 1.2")),
+                    () -> assertTrue(curPay.isCurCodeInAccess("Czk -1.222222145")));
         }
     }
 }
