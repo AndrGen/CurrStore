c73d72a852e76b9a3324b8c9182b0a3dc13bba5e
AndrGen <31141640+AndrGen@users.noreply.github.com>
2017-12-10 12:41:59 +0300

    main test

--- src/test/java/local/CurrStore/CTest.java
+++ src/test/java/local/CurrStore/CTest.java
@@ -1,15 +1,16 @@
 package local.CurrStore;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -114,7 +115,7 @@ class CTest {
 
         @org.junit.jupiter.api.Test
         @DisplayName("isCurCodeInAccess normal input")
-        void isCurCodeInAccessTestNormal() throws FileNotFoundException, ParseException  {
+        void isCurCodeInAccessTestNormal() throws FileNotFoundException, ParseException {
             curPay.initCurFromFile(fileNamePart);
             assertAll("isCurCodeInAccessTestNormal",
                     () -> assertTrue(curPay.isCurCodeInAccess("USD ")),
@@ -125,7 +126,7 @@ class CTest {
 
         @org.junit.jupiter.api.Test
         @DisplayName("isCurCodeInAccess wrong input")
-        void isCurCodeInAccessTestWrong() throws FileNotFoundException, ParseException  {
+        void isCurCodeInAccessTestWrong() throws FileNotFoundException, ParseException {
             curPay.initCurFromFile(fileNamePart);
             assertAll("isCurCodeInAccessTestWrong",
                     () -> assertFalse(curPay.isCurCodeInAccess("ddd ")),
@@ -135,7 +136,7 @@ class CTest {
 
         @org.junit.jupiter.api.Test
         @DisplayName("addToCurHashMapTest input")
-        void addToCurHashMapTest() throws FileNotFoundException, ParseException  {
+        void addToCurHashMapTest() throws FileNotFoundException, ParseException {
             curPay.initCurFromFile(fileNamePart);
             curPay.addToCurHashMap("aud", "ttt");
             curPay.addToCurHashMap("EUR", "100");
@@ -144,6 +145,43 @@ class CTest {
                     () -> assertTrue(currencyMap.containsKey("AUD")),
                     () -> assertEquals(new BigDecimal(0), currencyMap.get("AUD").getBill()),
                     () -> assertEquals(new BigDecimal(100), currencyMap.get("EUR").getBill()));
+        }
+    }
+
+    @Nested
+    @DisplayName("MainTest")
+    class MainTest {
+        @org.junit.jupiter.api.Test
+        @DisplayName("throws FileNotFoundException when Incorrect File Name")
+        void throwsExceptionWhenIncorrectFileName() {
+            assertThrows(FileNotFoundException.class, () -> new Main(""));
+        }
+
+        @org.junit.jupiter.api.Test
+        @DisplayName("throws ParseException when Incorrect currency name")
+        void throwsExceptionWhenIncorrectCurName() {
+            System.setIn(new ByteArrayInputStream("My string".getBytes()));
+            assertThrows(ParseException.class, () -> new Main(fileNamePart));
+        }
+
+
+        @org.junit.jupiter.api.Test
+        @DisplayName("main input")
+        void mainInput()  throws FileNotFoundException, ParseException {
+            String str = "RUB 18\nusd 1\nCNY -2.5\nhistory\n";
+
+            System.setIn(new ByteArrayInputStream(str.getBytes()));
+            try {
+                new Main(fileNamePart);
+            }
+            catch (NoSuchElementException nsee)
+            {
+            }
+            ConcurrentHashMap<String, Currency> currencyMap = CurPay.getCurrencyMap();
+            assertAll("mainInput",
+                    () -> assertTrue(currencyMap.containsKey("AUD")),
+                    () -> assertEquals(new BigDecimal(-2.5), currencyMap.get("CNY").getBill()),
+                    () -> assertEquals(new BigDecimal(9), currencyMap.get("RUB").getBill()));
         }
     }
 }
